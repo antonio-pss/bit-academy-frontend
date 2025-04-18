acacd2a373887b45b5ae1887805974c42d5b31c4
ithauana <ithauanasousad@gmail.com>
2025-04-17 21:25:17 -0400

    imp(#00-00):refactor-cod

--- src/app/templates/default-auth-layout/default-auth-layout.component.ts
+++ src/app/templates/default-auth-layout/default-auth-layout.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatCard} from '@angular/material/card';
-import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-default-auth-layout',
   imports: [
-    MatCard,
-    MatDivider
+    MatCard
   ],
   templateUrl: './default-auth-layout.component.html',
   styleUrl: './default-auth-layout.component.scss'
