81279a0ff348090139f68373220683a869cb6f0e
alef-monteiro <monteiro.alef0@gmail.com>
2025-04-29 19:07:37 -0400

    bugfix(#122841): Route refactored and main component to sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,34 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from './modules/auth/components/login/login.component';
 import {SignupComponent} from './modules/auth/components/signup/signup.component';
 import {SelectPlataformComponent} from './modules/auth/components/select-plataform/select-plataform.component';
-import {ClassroomComponent} from './modules/bit-class/components/classroom/classroom.component';
+import {MainComponent} from './modules/pages/main/main.component';
+import {HomeComponent} from './modules/auth/components/home/home.component';
 
 export const routes: Routes = [
+
   {
     path: 'auth',
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'signup', component: SignupComponent },
-      { path: 'platform-selector', component: SelectPlataformComponent },
-      // depois remover, ta aq p teste
-      { path: 'classroom', component: ClassroomComponent },
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {path: 'login', component: LoginComponent},
+      {path: 'signup', component: SignupComponent},
+      {path: 'platform-selector', component: SelectPlataformComponent},
+      {path: '', redirectTo: 'login', pathMatch: 'full'},
     ],
+  }, {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      }
+    ]
   },
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'auth/login' },
+  {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+  {path: '**', redirectTo: 'auth/login'},
 ];
