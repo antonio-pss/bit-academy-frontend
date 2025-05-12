b3741d36a29d7ba84533025af6b7c70a24952c63
ithauana <163068049+ithauana@users.noreply.github.com>
2025-05-11 23:23:13 -0400

    feat(#119868): Implements home class list component
    Co-authored-by: Álef Monteiro <alef.monteiro@fpf.br>

---
+++ src/app/modules/bit-class/components/home/class-list/class-card/class-card.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {DecimalPipe, NgClass} from '@angular/common';
+
+@Component({
+  selector: 'app-class-card',
+  imports: [
+    MatButton,
+    NgClass,
+    DecimalPipe,
+  ],
+  templateUrl: './class-card.component.html',
+  styleUrl: './class-card.component.scss'
+})
+export class ClassCardComponent {
+
+  @Input() title: string = '';
+  @Input() schedule: string = '';
+  @Input() studentCount: number = 0;
+  @Input() gradeAverage: number = 0;
+  @Input() assignmentsCount: number = 0;
+  @Input() progressPercentage: number = 0;
+  @Input() status: string = 'Active';
+  @Input() id: number = 0;
+
+}
---
+++ src/app/modules/bit-class/components/home/materials-list/materials-card/materials-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-materials-card',
+  imports: [],
+  templateUrl: './materials-card.component.html',
+  styleUrl: './materials-card.component.scss'
+})
+export class MaterialsCardComponent {
+
+}
