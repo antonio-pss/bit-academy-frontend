01877b9956fe7213ca5e231023fc53f29e2e64da
alef-monteiro <monteiro.alef0@gmail.com>
2025-05-19 20:55:46 -0300

    feat: Implement navigation method in class list component

--- src/app/modules/bit-class/components/home/class-list/class-form/class-form.component.ts
+++ src/app/modules/bit-class/components/home/class-list/class-form/class-form.component.ts
@@ -1,58 +1,15 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import {MatDialogRef} from '@angular/material/dialog';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {GeneralService} from '../../../../../../shared/services/general.service';
-import {EndpointsService} from '../../../../../../shared/services/endpoints.service';
-import {MATERIAL_IMPORTS} from '../../../../../../shared/imports/material.imports';
-import {Classroom} from '../../../../../../shared/models/class';
-
+import {MatDialogContainer} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-class-form',
   imports: [
-    ...MATERIAL_IMPORTS,
-    ReactiveFormsModule,
+    MatDialogContainer
   ],
-  standalone: true,
   templateUrl: './class-form.component.html',
   styleUrl: './class-form.component.scss'
 })
-export class ClassFormComponent  {
-
+export class ClassFormComponent {
   @Output() onClose = new EventEmitter<boolean>();
 
-  public classForm: FormGroup
-
-  constructor(
-    private readonly formBuilder: FormBuilder,
-    private readonly dialogRef: MatDialogRef<ClassFormComponent>,
-    private readonly classService: GeneralService,
-    private readonly endpoint: EndpointsService,
-  ) {
-    this.classForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required],
-      id_course_module_discipline: []
-    });
-  }
-
-  public closeDialog(success: boolean = false): void {
-    this.onClose.emit(success);
-    this.dialogRef.close(success);
-  }
-
-
-  public onSubmit(): void {
-    if (this.classForm.valid) {
-      this.classService.post(this.endpoint.path.class, this.classForm.value).subscribe({
-        next: () => {
-          this.closeDialog(true),
-            console.log(this.classForm.value);
-        },
-        error: (error: Error) => {
-          console.error('Erro ao criar classe:', error);
-        }
-      });
-    }
-  }
 }
--- src/app/shared/urls.ts
+++ src/app/shared/urls.ts
@@ -1,14 +1,12 @@
 const API_BASE_URL = 'http://127.0.0.1:8000/core';
-const API_BASE_DEAFAULT_URL = 'http://127.0.0.1:8000';
 
 export const URLS = {
   BASE: API_BASE_URL, // Pode ser alterado para a URL de produção quando necessário
   AUTH: {
     LOGIN: `${API_BASE_URL}/login/`,
     SIGNUP: `${API_BASE_URL}/signup/`,
-    REFRESH: `${API_BASE_URL}token/refresh/`,
   },
-  USER: `${API_BASE_URL}/user/`, // CRUD para usuários
-  CLASS: `${API_BASE_DEAFAULT_URL}/class/`, // CRUD para turmas
-  SCHOOL: `${API_BASE_DEAFAULT_URL}/school/`, // CRUD para escolas
+  USER: `${API_BASE_URL}/auser/`, // CRUD para usuários
+  CLASS: `${API_BASE_URL}/class/`, // CRUD para turmas
+  SCHOOL: `${API_BASE_URL}/school/`, // CRUD para escolas
 };
