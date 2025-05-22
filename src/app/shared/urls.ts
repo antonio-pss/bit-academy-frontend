dee49cfcdbc841b9f51393f487702d2628e3dc7f
Antônio Pereira da Silva Santos <79935176+antonio-pss@users.noreply.github.com>
2025-05-22 18:47:15 -0400

    fix endpoints

--- src/app/shared/urls.ts
+++ src/app/shared/urls.ts
@@ -1,13 +1,13 @@
-const API_BASE_URL = 'http://18.117.154.106/core';
+const API_BASE_URL = 'http://0.0.0.0:8000';
 
 export const URLS = {
   BASE: API_BASE_URL, // Pode ser alterado para a URL de produção quando necessário
   AUTH: {
-    LOGIN: `${API_BASE_URL}/login/`,
-    SIGNUP: `${API_BASE_URL}/signup/`,
-    REFRESH: `${API_BASE_URL}token/refresh/`,
+    LOGIN: `${API_BASE_URL}/core/login/`,
+    SIGNUP: `${API_BASE_URL}/core/signup/`,
+    REFRESH: `${API_BASE_URL}/core/token/refresh/`,
   },
-  USER: `${API_BASE_URL}/auser/`, // CRUD para usuários
+  USER: `${API_BASE_URL}/core/user/`, // CRUD para usuários
   CLASS: `${API_BASE_URL}/class/`, // CRUD para turmas
   SCHOOL: `${API_BASE_URL}/school/`, // CRUD para escolas
 };
