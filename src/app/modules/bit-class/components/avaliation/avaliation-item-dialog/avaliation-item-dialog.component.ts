036785be453b9c219d87099a2fef13d41f483a67
Alex <lexfisica@gmail.com>
2025-06-09 19:37:50 -0400

    WIP activity

---
+++ src/app/modules/bit-class/components/avaliation/avaliation-item-dialog/avaliation-item-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avaliation-item-dialog',
+  imports: [],
+  templateUrl: './avaliation-item-dialog.component.html',
+  styleUrl: './avaliation-item-dialog.component.scss'
+})
+export class AvaliationItemDialogComponent {
+
+}
